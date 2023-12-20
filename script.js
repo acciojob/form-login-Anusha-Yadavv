269fccc5de2e8f785c028d1a7d5f6a5d35d24b54
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-20 11:58:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	let formElement=document.getElementById("form1");
+	let firstName=formElement.elements["fname"].value;
+	let lastName=formElement.elements["lname"].value;
+	alert(`${firstName} ${lastName}`)
+	return false;
+	
 
 }
